c9a2159dcd9a31883da2feb0dd2fb754e96b1041
Sebastian Butter <sebastian.butter@gmx.at>
2017-08-09 09:17:33 +0200

    finished template and databinding fundamentals

--- template-data-binding/src/app/app.component.ts
+++ template-data-binding/src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'my-app',
+  template: `
+  	<h1>{{title}}</h1>
+  	<h2>My favourite hero is: {{myHero.name}}</h2>
+  	<p>Heroes: </p>
+  	<ul>
+  		<li *ngFor="let hero of heroes">
+  			{{hero.name}}
+  		</li>
+  	</ul>
+  	<p *ngIf="heroes.length > 3">There are many heroes!</p>
+  `
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Tour of Heroes';
+  heroes = [
+  	new Hero(1, 'Windstorm'),
+  	new Hero(13, 'Bombasto'),
+  	new Hero(15, 'Magneta'),
+  	new Hero(20, 'Tornado'),
+  ];
+  myHero = this.heroes[0];
 }
---
+++ template-data-binding/src/app/hero.ts
@@ -0,0 +1,8 @@
+export class Hero{
+	id: number;
+	name: string
+	constructor(id, name){
+		this.id = id;
+		this.name = name;
+	}
+}
